6822fda692ae5a631c591eb13179b05ab834fa32
jeff.dev <johan.06.92@gmail.com>
2022-02-01 14:29:06 +0100

    terminada el primer sprint

--- front/src/pages/menu_dia/menu_dia.vue
+++ front/src/pages/menu_dia/menu_dia.vue
@@ -1,68 +1,78 @@
 <template>
-    <h1>MENU DEL DIA</h1>   
-    <h2>Primeros</h2>
-    <section  v-for="menu in menus.primeros" :key="menu.id_dish">
-        <ul class="split_the_dishes">
-            <li>{{menu.name_dish}}  :</li> 
-            <li>{{menu.desc_dish}}</li>
-        </ul>
-    </section>
-        <h2>Segundos</h2>
-    <section  v-for="menu in menus.segundos" :key="menu.id_dish">
-        <ul class="split_the_dishes">
-            <li>{{menu.name_dish}}  :</li>
-            <li>{{menu.desc_dish}}</li>
-        </ul>
-    </section>
-        <h2>Postres</h2>
-    <section v-for="menu in menus.postres" :key="menu.id_dish">
-        <ul class="split_the_dishes">
-            <li>{{menu.name_dish}}  :</li>
-            <li>{{menu.desc_dish}}</li>
-        </ul>
-    </section>
+  <h1>MENU DEL DIA</h1>
+  <h2>Primeros</h2>
+  <section v-for="menu in this.primeros" :key="menu.id_dish">
+    <ul class="split_the_dishes">
+      <li>
+        Nombre: {{ menu.name_dish.replace(/^\w/, (c) => c.toUpperCase()) }}
+      </li>
+      <li>Descripción: {{ menu.desc_dish }}</li>
+    </ul>
+  </section>
+  <h2>Segundos</h2>
+  <section v-for="menu in this.segundos" :key="menu.id_dish">
+    <ul class="split_the_dishes">
+      <li>Nombre: {{ menu.name_dish }}</li>
+      <li>Descripción: {{ menu.desc_dish }}</li>
+    </ul>
+  </section>
+  <h2>Postres</h2>
+  <section v-for="menu in this.postres" :key="menu.id_dish">
+    <ul class="split_the_dishes">
+      <li>Nombre: {{ menu.name_dish }}</li>
+      <li>Descripción: {{ menu.desc_dish }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-    name:"menu_dia",
-    data() {
-      return {
-        menus: []
-      }
+  name: "menu_dia",
+  data() {
+    return {
+      menus: [],
+      primeros: [],
+      segundos: [],
+      postres: [],
+    };
+  },
+  computed: {
+    filteredMenus() {
+      return NaN;
     },
-    mounted() {
-      this.loadData()
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      const response = await fetch("http://localhost:5000/api/menu_dia");
+      this.menus = await response.json();
+
+      this.primeros = this.menus.desc.primeros;
+      this.segundos = this.menus.desc.segundos;
+      this.postres = this.menus.desc.postres;
     },
-    methods: {
-      async loadData() {
-        const response = await fetch('http://192.168.21.222:5000/api/menu_dia')
-        this.menus = await response.json()
-      }
-    }
-
-
-}
+  },
+};
 </script>
 
 <style scoped>
-*{
-    padding: 0;
+* {
+  padding: 0;
+  margin: 0;
 }
 
-
-li{
-    text-align: center;
-    list-style: none;
-    margin: 0.3em;
-};
-p{
-    margin: 200px ;
-    text-align: center;
-  
+li {
+  text-align: center;
+  list-style: none;
+  margin: 0.3em;
 }
-.split_the_dishes{
-    margin: 1.5em;
+p {
+  margin: 200px;
+  text-align: center;
 }
-
+.split_the_dishes {
+  margin: 1.5em;
+}
 </style>
