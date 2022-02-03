899093f85e8bad316fe09f0fc621a40194631499
jeff.dev <johan.06.92@gmail.com>
2022-02-03 10:05:50 +0100

    hemos añadido al fecth una condicipn para que nos de el menu de ese dia

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/menus">Menus</router-link> |
+    <router-link to="/menus">Menus</router-link>
   </div>
   <router-view />
 </template>
