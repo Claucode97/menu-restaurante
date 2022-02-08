962cb0e7f46ad23fe52602445878e599657c36c7
perla <aghata@gmail.com>
2022-02-08 13:11:38 +0100

    creacion de metodos para añadir menus a diccionario

--- front/src/pages/addmenu/AddMenu.vue
+++ front/src/pages/addmenu/AddMenu.vue
@@ -5,13 +5,12 @@
     </div>
     <p class="title">Nombre</p> <p class="title">Descripción</p>
     <p>Primeros:</p>
-    <section class="firsts">
-        1.<input class= "name_dish" type="text" placeholder="Introducir plato">
-        <input class= "desc_dish" type="text" placeholder="Introducir descripción">
-        2.<input class= "name_dish" type="text" placeholder="Introducir plato">
-        <input class= "desc_dish" type="text" placeholder="Introducir descripción">
-        3.<input class= "name_dish" type="text" placeholder="Introducir plato">
-        <input class= "desc_dish" type="text" placeholder="Introducir descripción">
+    <section class="firsts" v-for="i in 3" :key="i">
+        <pre>{{i}}</pre><input class= "name_dish" id="f1" type="text" placeholder="Introducir plato" v-model="name_dish" 
+        v-show= !block_input>
+        <input class= "desc_dish" type="text" placeholder="Introducir descripción" v-model="desc_dish" @keydown.tab="addDishToDict(i)" this.name_dish='' v-show= !block_input>
+        
+        
     </section>
     <p>Segundos:</p>
     <section class="seconds">
@@ -35,21 +34,43 @@
     </section>
     <button type="addNewMenu">Añadir Menú</button>
 </form>
+{{$data}}
 </template>
 <!-- /addNewMenu?date=2022-02-10 -->
 <script>
 export default {
   name: "",
   data() {
-    return { };
+    return {
+        block_input: false,
+        name_dish:'',
+        desc_dish:'',
+        firsts:[],
+        id_dish:'',
+        date:'',
+        id:''
+     };
   },
 
   mounted() {
     
   },
+  computed:{
+      isDisabled(){
+          return this.form.validated;
+      }
+  },
   methods: {
-    addNewMenu() { 
-        console.log('Trabajar en post new add menu')
+    addDish(){
+        console.log(this.firsts.length)
+    },
+    addDishToDict(number_dish) { 
+        let toDict={'name_dish':this.name_dish, 'desc_dish':this.desc_dish, 'id_dish':number_dish}
+        this.firsts.push(toDict)
+        this.block_input = true
+        this.name_dish=''
+        this.desc_dish=''
+
     },
   },
 };
