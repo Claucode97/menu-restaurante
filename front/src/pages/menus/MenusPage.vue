4894036a25e7e015ea23622d42fd3380ca18972e
josephhduvalgarcia <josephhduvalgarcia@gmail.com>
2022-02-10 08:50:13 +0100

    hemos agregado un boton que te lleve al menu del dia

--- front/src/pages/menus/MenusPage.vue
+++ front/src/pages/menus/MenusPage.vue
@@ -1,4 +1,7 @@
 <template>
+  <router-link class="a" :to="{name: 'Menu', params: {date: dameFecha}}">
+    <button> Menú del día</button>  
+  </router-link> 
   <ul v-for="menuDate in listOfDates" :key="menuDate.id">
     <router-link :to="{name: 'Menu', params: {date: menuDate.date}}">
       <li>{{ menuDate.date }}</li>
@@ -8,6 +11,7 @@
 
 
 <script>
+
 export default {
   name: "menu_dia",
   data() {
@@ -19,7 +23,27 @@ export default {
   mounted() {
     this.loadData();
   },
+  computed:{
+    dameFecha(){
+      let current = new Date();
+      let year = current.getFullYear();
+      let month = current.getMonth()+1;
+      let day = current.getDate();
+      if (day < 10 ){
+          day = "0" + day
+      }
+      if (month < 10 ){
+          month = "0" + month
+      }
+
+      const hoy = year+'-'+month+'-'+day;
+      return hoy
+
+    },
+  },
   methods: {
+    
+
     async loadData() {
       const response = await fetch("http://192.168.21.125:5000/api/menus");
       this.listOfDates = await response.json();
@@ -37,5 +61,14 @@ ul {
   list-style: none;
   text-align: center;
   margin: 1em;
+}
+
+a{
+  text-decoration: none;
+  
+}
+h1{
+  border: 2px solid black; 
+
 }
 </style>
