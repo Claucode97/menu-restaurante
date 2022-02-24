fdc63db977a5d23aa2d45f012d85cce7789999ab
josephhduvalgarcia <josephhduvalgarcia@gmail.com>
2022-02-24 10:19:52 +0100

    ya se puede iniciar el app.py y tira la tabla Restaurant

--- back/app.py
+++ back/app.py
@@ -2,13 +2,15 @@ import sqlite3
 from src.domain.Menu import MenuRepository
 from src.webserver import create_app
 from src.domain.info import InfoRepository
+from src.domain.Restaurant import RestaurantRepository
 
 
 database_path = "data/database.db"
 
 repositories = {
     "info": InfoRepository(database_path),
-    "menu": MenuRepository(database_path)
+    "menu": MenuRepository(database_path),
+    "restaurant": RestaurantRepository(database_path),
 }
 
 app = create_app(repositories)
--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -4,6 +4,7 @@ import json
 sys.path.insert(0, "")
 from src.domain.Menu import MenuRepository, Menu
 from src.example_menu import dict_menu, dict_menu_2, dict_menu_3
+from src.domain.Restaurant import RestaurantRepository, Restaurant
 
 
 database_path = "data/database.db"
@@ -15,3 +16,8 @@ menu_repository.save(Menu(id="MM", date="2022-01-03", desc=dict_menu))
 menu_repository.save(Menu(id="MT", date="2022-02-03", desc=dict_menu_2))
 
 menu_repository.save(Menu(id="MX", date="2022-03-03", desc=dict_menu_3))
+
+restaurant_reposotory = RestaurantRepository(database_path)
+
+restaurant_reposotory.save_restaurants(Restaurant("restaurant_01", "Josu"))
+restaurant_reposotory.save_restaurants(Restaurant("restaurant_02", "Joseba"))
